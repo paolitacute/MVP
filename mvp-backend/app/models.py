4c9c7d2088822f152e62c5705b5762dc2b8cacd6
paolitacute <154352626+paolitacute@users.noreply.github.com>
2026-07-11 21:57:42 -0400

    Delete mvp-backend directory

--- mvp-backend/app/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
-
-class Store(Base):
-    __tablename__ = "stores"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    instagram_handle = Column(String, unique=True, index=True)
